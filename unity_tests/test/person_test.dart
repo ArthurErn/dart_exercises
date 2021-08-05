b8645d33a4a9866160c980aa6b4c712058978e0f
sys-arthur <arthurern48@gmail.com>
2021-08-05 09:24:40 -0300

    unity_test

---
+++ unity_tests/test/person_test.dart
@@ -0,0 +1,34 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:unity_tests/person.dart';
+
+void main() {
+  final person = Person('Arthur', 19, 1.82, 85);
+
+  setUpAll(() {
+    print('iniciando suit...');
+  });
+
+  setUp(() {
+    print('\niniciando o teste\n');
+  });
+
+  tearDown(() {
+    print('finalizando teste, resultado:');
+  });
+
+  test("O imc deve vir '25.66'", () {
+    print('executando...\n');
+    expect(person.imc, equals(25.66));
+  });
+
+  test("maior que 18", () {
+    print('executando...\n');
+    expect(person.isOlder, true);
+  });
+
+  test("igual a 18", () {
+    print('executando...\n');
+    expect(person.isOlder, true);
+  });
+}
